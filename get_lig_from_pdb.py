2d475fc465b706c41bcc4bdfca821ac7dc8d0e50
abazabaaa <66654563+abazabaaa@users.noreply.github.com>
2021-03-14 15:43:41 -0400

    Add files via upload

---
+++ get_lig_from_pdb.py
@@ -0,0 +1,110 @@
+import sys
+from prody import *
+from pdbe import pyPDBeREST
+import json
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from io import StringIO
+from collections import defaultdict
+from rdkit.Chem import rdFMCS
+from rdkit.Chem import Draw
+from rdkit.Chem.Draw import IPythonConsole
+from rdkit.Chem import rdDistGeom
+from rdkit.Chem import rdMolAlign
+IPythonConsole.ipython_3d = True
+import py3Dmol
+from openff.toolkit.topology import Molecule
+from openff.toolkit.topology import Topology
+from openff.toolkit.utils import RDKitToolkitWrapper
+from openff.toolkit.typing.engines.smirnoff import ForceField
+from simtk import openmm
+from simtk import unit
+from rdkit.Chem import rdMolAlign
+import numpy as np
+from simtk import openmm, unit
+from datetime import timedelta
+from timeit import time
+
+def stopwatch(method):
+    def timed(*args, **kw):
+        ts = time.perf_counter()
+        result = method(*args, **kw)
+        te = time.perf_counter()
+        duration = timedelta(seconds=te - ts)
+        print(f"{method.__name__}: {duration}")
+        return result
+    return timed
+
+@stopwatch
+def drawit(m,p=None,confId=-1):
+        mb = Chem.MolToMolBlock(m,confId=confId)
+        if p is None:
+            p = py3Dmol.view(width=400,height=400)
+        p.removeAllModels()
+        p.addModel(mb,'sdf')
+        p.setStyle({'stick':{}})
+        p.setBackgroundColor('0xeeeeee')
+        p.zoomTo()
+        return p.show()
+
+
+@stopwatch
+def get_bound_ligand_from_pdb(pdb_id):
+    # get information associated with molecules from PDBe
+    p = pyPDBeREST()
+    molecule_info = p.PDB.getMolecules(pdbid=pdb_id)
+    
+    #get the pdb in a way similar to Pat's post
+    ag = parsePDB(pdb_id)
+    
+    #load the json into python from PDBe
+    compound_dict = json.loads(molecule_info)
+    
+    #use list comprehension to deal with the compounds in the json
+    compounds = [compound_dict[pdb_id][l] for l in range(len(compound_dict[pdb_id]))]
+    
+    #run a for loop and find the bound ligand
+    for entries in compounds:
+        if entries['molecule_type'] == 'bound':
+            ligand_code = entries['chem_comp_ids'][0]
+            break
+        else:
+            print('Not your ligand')
+            
+    #get the info associated with the bound ligand and return the SMILES
+    ligand_data = fetchPDBLigand(ligand_code)
+    template = AllChem.MolFromSmiles(ligand_data['OpenEye_OEToolkits_SMILES_CANONICAL'])
+    
+    #now we need the prody command to pull the ligand
+    arg = 'resname_{ligand_code}'.format(ligand_code = ligand_code)
+    
+    #use getattr to pass in arg the allow us to select the ligand in the pdb
+    ligand = getattr(ag, arg)
+    
+    #use stringIO to collect the pdb information for the ligand
+    output = StringIO()
+    writePDBStream(output, ligand)
+    pdb_string = output.getvalue()
+    
+    #make an RDKit mol
+    rd_mol = AllChem.MolFromPDBBlock(pdb_string)
+
+    #now the weird part.. I wanted to start with 3pbl, and ETQ always came out as a dimer. I bet
+    #this can be made simplier if new more about how prody worked, but this was an ok solution
+    #for now. Basically we are making some logic up.. if we get mol objects as dimers, split them
+    #and make sure our smiles matches up. 
+    
+    if len(AllChem.GetMolFrags(rd_mol)) == 1:
+        mol_w_bond_orders = AllChem.AssignBondOrdersFromTemplate(template, rd_mol)
+        mol = AllChem.AddHs(mol_w_bond_orders, addCoords=True)
+        return mol
+    else:
+        split_structures = Chem.GetMolFrags(rd_mol,asMols=True)
+        if template.GetNumAtoms() == split_structures[0].GetNumAtoms():
+            mol_w_bond_orders = AllChem.AssignBondOrdersFromTemplate(template, rs[0])
+            mol = AllChem.AddHs(mol_w_bond_orders, addCoords=True)
+            return mol
+        else:
+            raise ValueError()
+            print(f'template and identified mol object from {pdb_id} do not have the same number of atoms')
+
